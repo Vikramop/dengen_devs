e4fef0ac4c046dac48960f29d8ba0950909ee862
Veervikram singh <veervikramsingh132@gmail.com>
2023-10-16 19:15:00 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import MenuIcon from '../assets/menuicons;';
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+        <h1>Menu</h1>
+      </div>
+      <h1>DEGEN DEVS</h1>
+    </div>
+  );
+};
+
+export default Header;
